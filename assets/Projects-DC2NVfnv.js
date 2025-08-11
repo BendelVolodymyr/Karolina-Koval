edb8fb0f59dfcd65c696a6dcf93e225fe54fe97f
BendelVolodymyr <bendel.vova@gmail.com>
2025-08-11 20:49:24 +0200

    Build for GitHub Pages

---
+++ assets/Projects-DC2NVfnv.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-AP4_v9YH.js";const e=()=>s.jsx("div",{className:"p-4",children:"Мої роботи"});export{e as default};
--- assets/Projects-_5axjJCc.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-C7SWmAoT.js";const e=()=>s.jsx("div",{className:"p-4",children:"Мої роботи"});export{e as default};
