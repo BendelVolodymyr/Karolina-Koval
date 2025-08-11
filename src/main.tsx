f20163521ae390f5517cae94b6c9bdfb208cfd0a
BendelVolodymyr <bendel.vova@gmail.com>
2025-08-11 19:13:33 +0200

    test4

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 import { BrowserRouter } from 'react-router-dom';
-// basename="/Karolina-Koval" тест
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Karolina-Koval">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
